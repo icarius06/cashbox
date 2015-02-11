ac7080a957bbc721a4b44460a20b66669c8b34b7
Michael Otieno <icarius06@gmail.com>
2015-02-11 11:24:39 +0300

    message

--- app/src/main/java/agronet/mpayer/zegetech/com/mpayercashbox/MembersActivity.java
+++ app/src/main/java/agronet/mpayer/zegetech/com/mpayercashbox/MembersActivity.java
@@ -1,21 +1,40 @@
 package agronet.mpayer.zegetech.com.mpayercashbox;
 
+import android.content.pm.ApplicationInfo;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import agronet.mpayer.zegetech.com.mpayercashbox.adapters.LazyAdapter;
 
 
 public class MembersActivity extends ActionBarActivity {
+
+
+
+    private List<ApplicationInfo> mAppList;
+    AppAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +51,13 @@ public class MembersActivity extends ActionBarActivity {
         mTitle.setTypeface(face);
         mTitle.setTextSize(30);
         mTitle.setText("Members");
-       /* ListView list=(ListView)findViewById(R.id.list);
+        SwipeMenuListView listView=(SwipeMenuListView)findViewById(R.id.listView);
 
-        ArrayList<HashMap<String,String>> data  = new ArrayList<HashMap<String, String>>();
+        mAppList = getPackageManager().getInstalledApplications(0);
+        mAdapter = new AppAdapter();
+        listView.setAdapter(mAdapter);
+
+        /*ArrayList<HashMap<String,String>> data  = new ArrayList<HashMap<String, String>>();
         HashMap<String,String>person = new HashMap<String, String>();
         person.put("name","Andrew Munya");
         person.put("id","254713245463");
@@ -48,11 +71,61 @@ public class MembersActivity extends ActionBarActivity {
         person = new HashMap<String, String>();
         person.put("name","Berth Babu");
         person.put("id","beverage@karizshop.com");
-        data.add(person);
+        data.add(person);*/
 
         // Getting adapter by passing xml data ArrayList
-        LazyAdapter adapter=new LazyAdapter(this, data);*/
-        //list.setAdapter(adapter);
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(dp2px(90));
+                // set item title
+                openItem.setTitle("Topup");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(R.color.primaryColor);
+                // set item width
+                deleteItem.setWidth(dp2px(90));
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_close);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+// set creator
+        listView.setMenuCreator(creator);
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        // open
+                        break;
+                    case 1:
+                        // delete
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
     }
 
     @Override
@@ -75,5 +148,53 @@ public class MembersActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class AppAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mAppList.size();
+        }
+
+        @Override
+        public ApplicationInfo getItem(int position) {
+            return mAppList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = View.inflate(getApplicationContext(),
+                        R.layout.item_list_app, null);
+                new ViewHolder(convertView);
+            }
+            ViewHolder holder = (ViewHolder) convertView.getTag();
+            ApplicationInfo item = getItem(position);
+            holder.iv_icon.setImageDrawable(item.loadIcon(getPackageManager()));
+            holder.tv_name.setText(item.loadLabel(getPackageManager()));
+            return convertView;
+        }
+
+        class ViewHolder {
+            ImageView iv_icon;
+            TextView tv_name;
+
+            public ViewHolder(View view) {
+                iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+                tv_name = (TextView) view.findViewById(R.id.tv_name);
+                view.setTag(this);
+            }
+        }
+    }
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getResources().getDisplayMetrics());
     }
 }
